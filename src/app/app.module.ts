3ca2f5461399bfc86ac96d1a2b2b91f5dd3f2a1d
Elías Gago <egago@fidesol.org>
2017-05-08 15:13:40 +0200

    http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 
@@ -17,6 +22,8 @@ import { HeroDetailComponent } from './hero-detail.component';
   imports:      [ 
   	BrowserModule,
   	FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ 
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -33,6 +33,28 @@ export class HeroesComponent implements OnInit  {
 		);
 	}
 
+	add(name: string): void {
+		name = name.trim();
+		if(!name){ return };
+		this.heroService.create(name).then(
+			hero => {
+				this.heroes.push(hero);
+				this.selectedHero = null;
+			}
+		)
+	}
+
+	delete(hero: Hero): void {
+		this.heroService.delete(hero.id).then(
+			() => {
+				this.heroes = this.heroes.filter(h => h !== hero);
+				if(this.selectedHero === hero) { 
+					this.selectedHero = null;
+				}
+			}
+		)
+	}
+
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
